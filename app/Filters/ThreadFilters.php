59099c041d99644757db1122bcaa3592f9fe719e
dannyconnolly <daniel@tutza.com>
2017-10-25 21:41:55 +0100

    17-A-User-Can-Filter-Threads-By-Popularity

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -6,12 +6,35 @@ use App\User;
 
 class ThreadFilters extends Filters
 {
-    protected $filters = ['by'];
+    /**
+     * Registerd filters to operate on
+     * 
+     * @var array
+     */
+    protected $filters = ['by', 'popular'];
 
+    /**
+     * Filter the query by a given username
+     * 
+     * @param $username
+     * @return $this;
+     */
     public function by($username)
     {
         $user = User::where('name', $username)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+    }
+
+    /**
+     * Filter the query according to most popular threads
+     * 
+     * @return $this;
+     */
+    public function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+        
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -11,12 +11,18 @@
                     @foreach ($threads as $thread)
 
                     <article>
-                        <h4>
-                            <a href="{{ $thread->path() }}">
-                                {{ $thread->title }}
-                            </a>
-                        </h4>
+                        <div class="level">
+                            <h4 class="flex">
+                                <a href="{{ $thread->path() }}">
+                                    {{ $thread->title }}
+                                </a>
+                            </h4>
+
+                            <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+                        </div><!-- /.level -->
+                        
                         <div class="body">{{ $thread->body }}</div>
+
                     </article>
 
                     <hr>
