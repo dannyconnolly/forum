3df50a14ef4d0941b1e5628c39d5e22aa08e3378
dannyconnolly <daniel@tutza.com>
2017-10-26 16:43:55 +0100

    22-A-User-Has-A-Profile

--- app/User.php
+++ app/User.php
@@ -26,4 +26,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the route key name for Laravel
+     * 
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
+
 }
