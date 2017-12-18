e90ddc4e5e22e8d7245b64fe334b6bf8c6dd7369
dannyconnolly <daniel@tutza.com>
2017-12-18 15:44:35 +0000

    27-Extracting-a-Controller-Query-to-the-Model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Activity;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
+    /**
+     * Show the users profile
+     * 
+     * @param App\User $user
+     * @return \Response
+     */
     public function show(User $user)
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => \App\Activity::feed()
+            'activities' => Activity::feed($user)
         ]);
     }
-
-    public function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(10)->get()->groupBy(function ($activity){
-            return $activity->created_at->format('Y-m-d');
-        });
-    }
+    
 }
