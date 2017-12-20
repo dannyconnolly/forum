3fa43cd7cc0f08e38003e0a78d186d25588c39e6
dannyconnolly <daniel@tutza.com>
2017-12-20 13:00:34 +0000

    36-Extracting-Components-With-Blade-Functionality

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,62 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
 
-                    <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: 
-                            {{ $thread->title }}
-                        </span>
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: 
+                                    {{ $thread->title }}
+                                </span>
 
-                        @can ('update', $thread)
-                            <form action="{{ $thread->path() }}" method="POST">
-                                {{ csrf_field() }}
+                                @can ('update', $thread)
+                                    <form action="{{ $thread->path() }}" method="POST">
+                                        {{ csrf_field() }}
 
-                                {{ method_field('DELETE') }}
+                                        {{ method_field('DELETE') }}
 
-                                <button type="submit" class="btn btn-link">Delete Thread</button>
-                            </form>
-                        @endcan
-                    </div><!-- /.level -->
-                </div>
+                                        <button type="submit" class="btn btn-link">Delete Thread</button>
+                                    </form>
+                                @endcan
+                            </div><!-- /.level -->
+                        </div><!-- /.panel-heading -->
 
-                <div class="panel-body">
-                    {{ $thread->body }}
-                </div>
-            </div>
+                        <div class="panel-body">
+                            {{ $thread->body }}
+                        </div><!-- /.panel-body -->
+                    </div><!-- /.panel panel-default -->
 
-            @foreach ($replies as $reply)
-                @include ('threads.reply')
-            @endforeach
+                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+                    
+                    {{--  {{ $replies->links() }}  --}}
 
-            {{ $replies->links() }}
+                    @if (auth()->check())
+                        <form action="{{ $thread->path() . '/replies' }}" method="POST" role="form">
+                            {{ csrf_field() }}        
+                            <div class="form-group">
+                                <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>
+                            </div>
+                        
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    @else
+                        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                    @endif
+                </div><!-- /.col-md-8 -->
 
-            @if (auth()->check())
-                <form action="{{ $thread->path() . '/replies' }}" method="POST" role="form">
-                    {{ csrf_field() }}        
-                    <div class="form-group">
-                        <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>
-                    </div>
-                
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-            @else
-                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-            @endif
-        </div>
-
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="#">{{ $thread->creator->name }}</a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by 
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently has 
+                            <span v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}.
+                        </div><!-- /.panel-body -->
+                    </div><!-- /.panel panel-default -->
+                </div><!-- /.col-md-4 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </thread-view>
 @endsection
