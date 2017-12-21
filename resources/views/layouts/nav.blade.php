a0e81e5772faf913291793868e56b5874fa9d1cb
dannyconnolly <daniel@tutza.com>
2017-12-21 11:27:25 +0000

    39-A-User-Can-Filter-By-Unanswered-Threads

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -25,12 +25,8 @@
 
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="/threads">All Threads</a></li>
-
-                        @if (auth()->check())
-                            <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
-                        @endif
-
                         <li><a href="/threads?popular=1">Popular Threads</a></li>
+                        <li><a href="/threads?unanswered=1">Unanswered Threads</a></li>
                     </ul>
                 </li>
                 <li><a href="/threads/create">New Thread</a></li>
