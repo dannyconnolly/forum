a853784afb97d411f579d20f506dcdebfca9bfc2
dannyconnolly <daniel@tutza.com>
2018-02-21 23:17:57 +0000

    62-Basic-UI-Tweaks

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,31 @@
+@forelse ($threads as $thread)
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="level">
+                <div class="flex">
+                    <h4>
+                        <a href="{{ $thread->path() }}">
+                            @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                            <strong>{{ $thread->title }}</strong>
+                            @else
+                            {{ $thread->title }}
+                            @endif
+                        </a>
+                    </h4>
+                    
+                    <h5>
+                        Posted By: <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
+                    </h5>
+                </div><!-- /.flex -->
+
+                <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+            </div><!-- /.level -->
+        </div><!-- /.panel-heading -->
+
+        <div class="panel-body">                        
+            <div class="body">{{ $thread->body }}</div>
+        </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
+@empty
+    <p>There are no relevant results</p>
+@endforelse
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,35 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @forelse ($threads as $thread)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div class="level">
-                        <h4 class="flex">
-                            <a href="{{ $thread->path() }}">
-                                @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                    <strong>{{ $thread->title }}</strong>
-                                @else
-                                    {{ $thread->title }}
-                                @endif
-                            </a>
-                        </h4>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @include('threads._list')
 
-                        <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
-                    </div><!-- /.level -->
-                </div><!-- /.panel-heading -->
-
-                <div class="panel-body">                        
-                    <div class="body">{{ $thread->body }}</div>
-                </div><!-- /.panel-body -->
-            </div><!-- /.panel -->
-            @empty
-                <p>There are no relevant results</p>
-            @endforelse
-        </div><!-- /.col-md-8 -->
-    </div><!-- /.row -->
-</div><!-- /.container -->
+                {{ $threads->render() }}
+            </div><!-- /.col-md-8 -->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
 @endsection
