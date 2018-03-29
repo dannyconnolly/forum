51f9a81cb773b8a59dbacf44a072979195b99e72
dannyconnolly <daniel@tutza.com>
2018-03-29 11:20:13 +0100

    73-Users-Must-Confirm-Their-Email-Address-3-Cleanup

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -17,10 +16,6 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\ThreadReceivedNewReply' => [
             'App\Listeners\NotifyMentionedUsers',
             'App\Listeners\NotifySubscribers',
-        ],
-
-        Registered::class => [
-            'App\Listeners\SendEmailConfirmationRequest',
         ]
     ];
 
