1aae07d50de4f3361ae5c762dcc4c5a5fd228004
dannyconnolly <daniel@tutza.com>
2018-03-29 11:43:49 +0100

    74-Loose-Ends

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -10,16 +10,14 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        }
-        catch (\Exception $e) {
-            return redirect(route('threads'))
-                ->with('flash', 'Unknown token.');
+        $user = User::where('confirmation_token', request('token'))->first();
+
+        if (!$user) {
+            return redirect(route('threads'))->with('flash', 'Unknown token.');
         }
 
+        $user->confirm();
+        
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed. You may post to the forum');
     }
