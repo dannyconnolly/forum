c7fca1091434c27e0612e84e5a8644e7b25ccd10
dannyconnolly <daniel@tutza.com>
2018-03-29 16:33:28 +0100

    80-Refactoring-Authorizations

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -54,12 +54,20 @@ if (token) {
 
 window.Vue = require('vue');
 
-Vue.prototype.authorize = function (handler) {
-    
-    let user = window.App.user;
+let authoizations = require('./authorizations');
 
-    return user ? handler(user) : false;
-}
+Vue.prototype.authorize = function (...params) {
+    
+    if (! window.App.signedIn) return false;
+
+    if (typeof params[0] === 'string') {
+        return authoizations[params[0]](params[1]);
+    }
+
+    return params[0](window.App.user);
+};
+
+Vue.prototype.signedIn = window.App.signedIn;
 
 window.events = new Vue();
 
