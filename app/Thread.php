ebdc87b4357cfbba4ddf75408a61df78f84e74ec
dannyconnolly <daniel@tutza.com>
2018-03-29 12:05:24 +0100

    75-Thread-URIs-Should-Use-Slugs-Instead-of-Ids

--- app/Thread.php
+++ app/Thread.php
@@ -52,7 +52,7 @@ class Thread extends Model
      */
     public function path()
     {
-        return "/threads/{$this->channel->slug}/{$this->id}";
+        return "/threads/{$this->channel->slug}/{$this->slug}";
     }
 
     /**
@@ -163,5 +163,10 @@ class Thread extends Model
         $key = $user->visitedThreadCacheKey($this);
 
         return $this->updated_at > cache($key);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
