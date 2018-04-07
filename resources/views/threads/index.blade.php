83a327abede14e27c81a5bce7350b68fa5ebbe34
dannyconnolly <daniel@tutza.com>
2018-04-07 11:58:35 +0100

    95-First-Class-Search-Part-2-Implementation

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,6 +10,25 @@
             </div><!-- /.col-md-8 -->
 
             <div class="col-md-4">
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Search
+                    </div><!-- /.panel-heading -->
+
+                    <div class="panel-body">
+                        <form action="/threads/search" method="GET">
+                            <div class="form-group">
+                                <input type="text" placeholder="Search for something..." name="q" class="form-control"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <button class="btn btn-default" type="submit">Search</button>
+                            </div>
+                        </form>
+                    </div><!-- /.panel-body -->
+                </div><!-- /.panel.panel-default -->
+
                 @if (count($trending))
                     <div class="panel panel-default">
                         <div class="panel-heading">
